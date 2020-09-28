801d80dfa089a12c77bdb43d0ef1c038bfd10497
devnishanth <67795990+devnishanth@users.noreply.github.com>
2020-09-28 18:18:40 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React , { Component } from 'react';
+import React ,{ Component } from 'react';
 import Navigation from './components/navigation/navigation';
 import './App.css';
 import Clarifai from 'clarifai';
